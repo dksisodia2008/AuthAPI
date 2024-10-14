b2d86793475679eec417964705693d9c4ab95321
Dheeraj K <dksisodia2008@gmail.com>
2024-10-14 19:32:54 +0530

    Auth API for Angular App

---
+++ Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using AuthAPI.API.Data;
+using AuthAPI.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+
+namespace AuthAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private AppDbContext _appDbContext;
+        public UserController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpPost]
+        [Route("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
+        {
+            
+            if (string.IsNullOrEmpty(userObj.UserName))
+            {
+                return BadRequest();
+            }
+            var user = await _appDbContext.Users.FirstOrDefaultAsync(u => u.UserName == userObj.UserName && u.Password == userObj.Password);
+
+            if (user == null)
+            {
+                return NotFound(new { Message= "User Not Found!" });
+            }
+            return Ok(new { Message = "Login Successfully!" });
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Registration([FromBody] User userObj)
+        {
+
+            if (string.IsNullOrEmpty(userObj.UserName))
+            {
+                return BadRequest();
+            }
+            await _appDbContext.Users.AddAsync(userObj);
+            await _appDbContext.SaveChangesAsync();
+            return Ok(new { Message = "Record Save Successfully!" });
+        }
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using AuthAPI.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AuthAPI.API.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users");
+        }
+    }
+}
